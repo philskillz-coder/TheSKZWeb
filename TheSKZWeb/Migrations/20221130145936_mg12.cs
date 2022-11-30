edcd90a0ed5fae0b41e2ffd767e8fede85ce5924
Philskillz <76884393+philskillz-coder@users.noreply.github.com>
2022-11-30 16:59:56 +0100

    Migrations

---
+++ TheSKZWeb/Migrations/20221130145936_mg12.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheSKZWeb.Migrations
+{
+    public partial class mg12 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Permissions_Permissions_ParentId",
+                table: "Permissions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Permissions_ParentId",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "IsCritical",
+                table: "Roles");
+
+            migrationBuilder.DropColumn(
+                name: "IsCritical",
+                table: "Permissions");
+
+            migrationBuilder.DropColumn(
+                name: "ParentId",
+                table: "Permissions");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCritical",
+                table: "Roles",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsCritical",
+                table: "Permissions",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "ParentId",
+                table: "Permissions",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Permissions_ParentId",
+                table: "Permissions",
+                column: "ParentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Permissions_Permissions_ParentId",
+                table: "Permissions",
+                column: "ParentId",
+                principalTable: "Permissions",
+                principalColumn: "Id");
+        }
+    }
+}
---
+++ TheSKZWeb/Migrations/20221130152315_mg13.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TheSKZWeb.Migrations
+{
+    public partial class mg13 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "MFAEnabled",
+                table: "Users",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MFAEnabled",
+                table: "Users");
+        }
+    }
+}
